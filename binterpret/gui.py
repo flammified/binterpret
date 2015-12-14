cfe0bac39b50faae4121dae42e20070688f95177
Alexander Freeman <rubberduck313@gmail.com>
2015-12-14 22:19:48 +0100

    Added checking if kivy is installed

--- binterpret/gui.py
+++ binterpret/gui.py
@@ -1,5 +1,9 @@
-from kivy.app import App
-from kivy.uix.widget import Widget
+try:
+    from kivy.app import App
+    from kivy.uix.widget import Widget
+except ImportError as e:
+    print("Kivy is not installed in your environment. --gui can not be used.")
+    exit(1)
 
 class BinterpretWidget(Widget):
     pass
