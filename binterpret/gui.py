647ce698397695d28191e999d765ac70184e6620
Alexander Freeman <rubberduck313@gmail.com>
2015-12-15 12:26:43 +0100

    Continued work on GUI

--- binterpret/gui.py
+++ binterpret/gui.py
@@ -1,13 +1,16 @@
 try:
+    from kivy.uix.floatlayout import FloatLayout
     from kivy.app import App
-    from kivy.uix.widget import Widget
+    from kivy.properties import ObjectProperty, StringProperty
+    from kivy.core.window import Window
 except ImportError as e:
     print("Kivy is not installed in your environment. --gui can not be used.")
     exit(1)
 
-class BinterpretWidget(Widget):
+class BinterpretWidget(FloatLayout):
     pass
 
 class BinterpretApp(App):
     def build(self):
+        Window.size = (750 , 400)
         return BinterpretWidget()
